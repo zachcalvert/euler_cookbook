8566df8059d9a842c83de06ea1a6a159cabb5a98
Zach Calvert (zcalvert) <zcalvert@uoregon.edu>
2015-06-22 22:03:18 -0700

    rewrite management script to query cookbook API

--- problems/management/commands/load_problems.py
+++ problems/management/commands/load_problems.py
@@ -8,29 +8,30 @@ from problems.models import Problem
 
 class Command(BaseCommand):
     """
-    Management command to load problem info from project euler site into the db
+    Management command to load problem info from the euler cookbook API into a local db
     """
     def handle(self, *args, **options):
-    	for i in range(1,510):
-    		url = 'https://projecteuler.net/problem={}'.format(i)
-    		response = requests.get(url, verify=False)
+        for i in range(1,510):
+            url = 'http://eulerscookbook.org/problem?number={}'.format(i)
+            response = requests.get(url)
+            content = json.loads(response.content)
+            print(content)
+            title = content.get('title')
+            description = content.get('description')
+            solved = content.get('solved')
 
-    		soup = BeautifulSoup(response.text)
+            if solved:
+                solution = content.get('solution')
+            else:
+                solution = None
 
-    		title_html = soup.find('h2')
-    		title = title_html.text
-
-    		problem_html = soup.find('div', class_='problem_content')
-    		description = problem_html.text
-
-    		problem, created = Problem.objects.get_or_create(
-    			number=i,
-    			title=title,
-    			description=description,
-    			link=url,
-    		)
-    		if created:
-    			try:
-    				print('added problem {0}: {1}'.format(i, title))
-    			except UnicodeEncodeError:
-    				print('added problem {}'.format(i))
+            problem, created = Problem.objects.get_or_create(
+                number=i,
+                title=title,
+                description=description,
+                link='https://projecteuler.net/problem={}'.format(i),
+                solved=solved,
+                solution=solution
+            )
+            if created:
+                print('added problem {0}: {1}'.format(i, title))
