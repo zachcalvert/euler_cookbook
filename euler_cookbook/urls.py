78bf1b6bb09464c621bc826a89bfa5fc298a5131
Zach Calvert <zachc@darkhorse.com>
2015-06-22 23:24:17 -0700

    utilize django rest framework to serve problem info

--- euler_cookbook/urls.py
+++ euler_cookbook/urls.py
@@ -2,8 +2,19 @@ from django.conf.urls import include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
 
+from rest_framework import routers, viewsets
+
+from problems.serializers import UserViewSet, ProblemViewSet
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'problems', ProblemViewSet)
+
 urlpatterns = [
 	url(r'^', include('problems.urls')),
 	url(r'^accounts/', include('accounts.urls')),
+	url(r'^api/', include(router.urls)),
+	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 	url(r'^admin/', include(admin.site.urls)),
 ]
